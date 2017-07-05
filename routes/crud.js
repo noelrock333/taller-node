78157d8904955498685915c5a72c9d4be9f12fd3
Noel Escobedo <noel@Noels-MacBook-Pro.local>
2017-07-05 23:18:33 +0000

    Subiendo archivo usando multer

--- routes/crud.js
+++ routes/crud.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var bookshelf = require('../bookshelf');
 var knex = bookshelf.knex;
+var multer = require('multer');
+var subidas = multer({ dest: 'subidas/' })
 
 /* GET home page. */
 router.get('/nuevo', function(req, res, next) {
@@ -17,6 +19,17 @@ router.post('/guardar', function(req, res, next) {
       res.json(reg[0]);
     });
   });
+});
+
+router.get('/cargar', function(req, res, next) {
+  res.render('CRUD_views/cargar');
+});
+
+router.post('/cargar', subidas.single('archivo'), function(req, res, next) {
+  if(req.file) {
+    return res.send('Archivo enviado');
+  }
+  return res.send('Ha ocurrido un error');
 });
 
 module.exports = router;
