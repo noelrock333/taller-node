93343c4218ae65924675cb5e03654cff256f8ad8
Noel Escobedo <noel_ska15@hotmail.com>
2017-07-04 23:49:01 -0500

    Enviando cosas al backend

--- routes/crud.js
+++ routes/crud.js
@@ -2,11 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('CRUD_views/new', {
-  	titulo: 'Taller de node.js',
-  	saludo: 'Hola desde node yey!'
+router.get('/nuevo', function(req, res, next) {
+  res.render('CRUD_views/nuevo', {
+    titulo: 'Taller de node.js',
+    saludo: 'Hola desde node yey!'
   });
+});
+
+router.post('/guardar', function(req, res, next) {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 module.exports = router;
