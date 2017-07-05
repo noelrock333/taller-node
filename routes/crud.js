bec9f124c3130e1beefe793ba908b8f8c144f052
Noel Escobedo <noel_ska15@hotmail.com>
2017-07-04 23:09:51 -0500

    Primer vista personalizada

--- routes/crud.js
+++ routes/crud.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('CRUD_views/new', {
+  	titulo: 'Taller de node.js',
+  	saludo: 'Hola desde node yey!'
+  });
 });
 
 module.exports = router;
