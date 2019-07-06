2643ad727fe4441da593eb819765219ac2389a40
Noel Escobedo <noel@michelada.io>
2019-07-06 18:29:28 +0000

    Fetch fix

--- routes/videos.js
+++ routes/videos.js
@@ -11,7 +11,7 @@ router.post('/create', function(req, res, next) {
 });
 
 router.get('/show/:id', function(req, res, next) {
-  new Video().fetchOne({ id: req.params.id }).then(model => {
+  new Video().fetch({ id: req.params.id }).then(model => {
     res.json(model.toJSON());
   });
 });
